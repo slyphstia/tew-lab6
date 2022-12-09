280223ecbe7e1517c789465426faa4f412d6ccaf
sylphstia <apiwit_bppnyarit@cmu.ac.th>
2022-12-09 16:21:59 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main(){ 
+    int A=0;
+    int B=0;
+    int N=1;  
+    while(N!=0){
+        cout << "Enter an integer: ";
+        cin >> N ;
+    if(N%2==0){
+        A=A+1; 
+    }
+    else{
+        B=B+1;
+    }
+    }
+    cout << "#Even numbers = " <<A-1<<"\n";
+    cout << "#Odd numbers = " <<B;
     return 0;
 }
